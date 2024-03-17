c90116ca090c9474edebcbc5e824abf99d607c28
snijdersandreu <106550383+snijdersandreu@users.noreply.github.com>
2024-03-17 17:06:22 +0100

    primeras funciones

--- primos.py
+++ primos.py
@@ -9,9 +9,6 @@ Tests unitarios:
 >>> primos(50)
 (2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47)
 
->>> primos(8)
-(2, 3, 5, 7)
-
 >>> descompon(36 * 175 * 143)
 (2, 2, 3, 3, 5, 5, 7, 11, 13)
 
@@ -30,29 +27,50 @@ Tests unitarios:
 """
 
 def esPrimo(numero):
-    return #True or False
+    """Determina si un número es primo."""
+    if numero < 2:
+        return False
+    for n in range(2, int(numero**0.5) + 1):
+        if numero % n == 0:
+            return False
+    return True
 
 def primos(numero):
-    return #tupla con todos los numeros primos menores que su argumento
+    """Devuelve una tupla con todos los números primos menores que su argumento."""
+    return tuple(numero for numero in range(2, numero) if esPrimo(numero))
 
 def descompon(numero):
-    return #tupla con la descomposición en factores primos de su argumento
-
-def mcm(numero1, numero2):
-    return #num min comu mult.
+    """Devuelve una tupla con la descomposición en factores primos de su argumento."""
+    i = 2
+    factores = []
+    while i * i <= numero:
+        if numero % i:
+            i += 1
+        else:
+            numero //= i   ## se usa //= en lugar de /= para assegurarse que la variable es de tipo entero y no se transforma a float, mas eficiente ya que siempre serà un entero
+            factores.append(i)
+    if numero > 1:
+        factores.append(numero)
+    return tuple(factores)
 
 def mcd(numero1, numero2):
-    return #num max comu div.
+    """Devuelve el número máximo común divisor."""
+    return ###
+
+def mcm(numero1, numero2):
+    """Devuelve el número mínimo común múltiplo."""
+    return ###
 
 def mcmN(*numeros):
-    # fot lo mateix que mcm() però *numeros és una tupla
-    return #num min comu mult.
+    """Devuelve el mínimo común múltiplo de sus argumentos."""
+    return ###
 
 def mcdN(*numeros):
-    # fot lo mateix que mcd() però *numeros és una tupla
-    return # num max comu div.
+    """Devuelve el máximo común divisor para un número arbitrario de argumentos."""
+    return ###
 
 if __name__ == "__main__":
     import doctest
     doctest.testmod(verbose=True)
+
 
